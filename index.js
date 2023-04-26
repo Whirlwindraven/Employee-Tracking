9639a3a8dbd364cb424f6d2fce18bd30063b9c87
Whirlwindraven <fadysherif1998@gmail.com>
2023-04-25 22:00:41 -0400

    added the prompting code to index.js

--- index.js
+++ index.js
@@ -12,3 +12,67 @@ const {
 } = require("./dbFunctions");
 
 dotenv.config();
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: "employee_db",
+  });
+  
+  connection.connect((err) => {
+    if (err) throw err;
+    console.log("Connected to the database!");
+    mainMenu();
+  });
+
+
+const mainMenu = () => {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "action",
+          message: "Choose an action:",
+          choices: [
+            "View all departments",
+            "View all roles",
+            "View all employees",
+            "Add a department",
+            "Add a role",
+            "Add an employee",
+            "Update an employee role",
+            "Exit",
+          ],
+        },
+      ])
+      .then((answers) => {
+        switch (answers.action) {
+          case "View all departments":
+            viewAllDepartments(connection, mainMenu);
+            break;
+          case "View all roles":
+            viewAllRoles(connection, mainMenu);
+            break;
+          case "View all employees":
+            viewAllEmployees(connection, mainMenu);
+            break;
+          case "Add a department":
+            addDepartment(connection, mainMenu);
+            break;
+          case "Add a role":
+            addRole(connection, mainMenu);
+            break;
+          case "Add an employee":
+            addEmployee(connection, mainMenu);
+            break;
+          case "Update an employee role":
+            updateEmployeeRole(connection, mainMenu);
+            break;
+          case "Exit":
+            connection.end();
+            break;
+        }
+      });
+  };
