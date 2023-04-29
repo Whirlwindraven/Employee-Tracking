2f7b91f1cbb8d483b85a86b491d3991a95a004b2
Whirlwindraven <fadysherif1998@gmail.com>
2023-04-29 17:20:08 -0400

    fixed .env error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
-const dotenv = require("dotenv");
+const dotenv = require("dotenv").config();
 const {
   viewAllDepartments,
   viewAllRoles,
